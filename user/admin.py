c4090ed4f5a2f46e43a472cc35a91f4153fb4d79
DmytriiTsiupko <tsiupkodmitrii@gmail.com>
2023-06-04 17:56:26 +0200

    Deleted some models

--- user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-
-@admin.register(User)
-class TelegramUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'first_name', 'last_name', 'phone_number')
