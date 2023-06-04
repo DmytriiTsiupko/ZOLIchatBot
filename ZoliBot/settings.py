fd294fd641869ed35fc6505d792f5bf49c5efe93
DmytriiTsiupko <tsiupkodmitrii@gmail.com>
2023-06-04 22:51:33 +0200

    Done for docker compose

--- ZoliBot/settings.py
+++ ZoliBot/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
@@ -75,7 +75,7 @@ DATABASES = {
         'NAME': 'ZoliDataBase',
         'USER': 'ZoliBot',
         'PASSWORD': 'ZoliBot',
-        'HOST': '0.0.0.0',
+        'HOST': 'database',
         'PORT': '5432',
     }
 }
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
