548876712b70618418270f7826203388b948a86f
DmytriiTsiupko <tsiupkodmitrii@gmail.com>
2023-06-04 18:00:39 +0200

    Deleted some models

--- language/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import Language
-from .serializers import LanguageSerializer
-
-
-class LanguageViewSet(viewsets.ModelViewSet):
-    queryset = Language.objects.all()
-    serializer_class = LanguageSerializer
